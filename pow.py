fcd83243b2dc502210347d84328ac6502aa26aa8
vitali-shevtsov <vitali.shevtsov@gmail.com>
2018-06-25 22:27:59 +0300

    Add script pow.py

---
+++ pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def pow(a:float, n:int):
+    if n==0:
+        return 1
+    elif n%2==1:
+        return pow(a,n-1)*a
+    else:
+        return pow(a**2,n//2)
+
+print(pow(5,5))
